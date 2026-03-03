5911cf7ecd95a26d6ca5a5637258bcdb6b150b25
Walterldk <waltersteven.dev@gmail.com>
2026-03-03 17:03:28 -0500

    ADD: se agrego el nuevo archivo de comunidad

---
+++ frontend/src/components/AulaVirtual/Comunidad.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Comunidad = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1 style={{ color: "#d32f2f" }}>Comunidad</h1>
+      <p>Foros de discusión y anuncios generales.</p>
+    </div>
+  );
+};
+
+export default Comunidad;
