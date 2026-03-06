6b9a013acfac1deeddda2ac2497bc9eb0cafaf23
Walterldk <waltersteven.dev@gmail.com>
2026-03-06 17:30:24 -0500

    FIX: se cambio el numero de WhatsApp del icono con el QR

--- frontend/src/components/WhatsApp/WhatsAppQR.jsx
+++ frontend/src/components/WhatsApp/WhatsAppQR.jsx
@@ -5,7 +5,7 @@ import styles from "./WhatsAppQR.module.css";
 
 const WhatsAppQR = () => {
     const [showScrollBtn, setShowScrollBtn] = useState(false);
-    const whatsappUrl = "https://wa.me/573234876604?text=Hola%20%F0%9F%91%8B%2C%20me%20interesa%20obtener%20m%C3%A1s%20informaci%C3%B3n%20sobre%20los%20servicios%20de%20Rutas%20del%20Saber.%20%C2%BFPodr%C3%ADan%20brindarme%20asesor%C3%ADa%20personalizada%3F%20Muchas%20gracias.";
+    const whatsappUrl = "https://wa.me/57000000000?text=Hola%20%F0%9F%91%8B%2C%20me%20interesa%20obtener%20m%C3%A1s%20informaci%C3%B3n%20sobre%20los%20servicios%20de%20Rutas%20del%20Saber.%20%C2%BFPodr%C3%ADan%20brindarme%20asesor%C3%ADa%20personalizada%3F%20Muchas%20gracias.";
     const qrImageUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(whatsappUrl)}`;
 
     useEffect(() => {
