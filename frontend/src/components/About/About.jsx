2147a656aecc00a3b954082dae1099cbf26bf871
Walterldk <waltersteven2206@gmail.com>
2026-03-14 13:51:35 -0500

    prueba de deploy de cpanel

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -129,11 +129,7 @@ const About = () => {
             <h2 className={styles.title3D}>¿QUIÉNES SOMOS?</h2>
             <div className={styles.divider}></div>
             <p className={styles.mainText}>
-              En Rutas del Saber, somos una plataforma líder dedicada a
-              transformar la educación vial y la convivencia armónica a través
-              de la tecnología. Creemos que el conocimiento es la herramienta
-              más poderosa para salvar vidas y construir una cultura de paz en
-              nuestras comunidades.
+              soy leonel messi
             </p>
           </div>
         </div>
