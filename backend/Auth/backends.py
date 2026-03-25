ec8ab080d2a966641d3e3541289bbeb1143ce742
samirdp04 <samirdelportillo01@gmail.com>
2026-03-25 14:54:14 -0500

    ADD - agregado nuevo archivo para colocar el email en lugar de username en la autenticación

---
+++ backend/Auth/backends.py
@@ -0,0 +1,19 @@
+# backends.py
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class EmailBackend(ModelBackend):
+    """
+    Autenticación usando email en lugar de username
+    """
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        email = kwargs.get('email', username)
+        
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
+        except User.DoesNotExist:
+            return None
