ba0bfc03ffbb10e05a75722e8b62e0c38a786013
samirdp04 <samirdelportillo01@gmail.com>
2026-03-25 14:55:19 -0500

    ADD - agregada rutas para de la api de autenticación

---
+++ backend/Auth/urls.py
@@ -0,0 +1,40 @@
+# tu_app/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import (LoginView, LogoutView, CurrentUserView, validar_padron, solicitar_acceso_alumno, solicitar_acceso_profesor, UserViewSet, ColegioViewSet, RequestPasswordReset, )
+
+# Configurar el router para los ViewSets
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'colegios', ColegioViewSet, basename='colegio')
+
+# URL Patterns
+urlpatterns = [
+    # ==================== AUTENTICACIÓN ====================
+    # Login
+    path('login/', LoginView.as_view(), name='login'),
+    
+    # Logout
+    path('logout/', LogoutView.as_view(), name='logout'),
+    
+    # Refresh Token
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # Usuario actual
+    path('me/', CurrentUserView.as_view(), name='current-user'),
+    
+    # ==================== VALIDACIÓN Y REGISTRO ====================
+    # Validar si existe en padrón
+    path('validar-padron/', validar_padron, name='validar-padron'),
+    
+    # Registro de alumnos
+    path('registro/alumno/', solicitar_acceso_alumno, name='registro-alumno'),
+    
+    # Registro de profesores
+    path('registro/profesor/', solicitar_acceso_profesor, name='registro-profesor'),
+
+    path('request-reset/', RequestPasswordReset.as_view(), name='cambiar-contraseña'),
+
+]
