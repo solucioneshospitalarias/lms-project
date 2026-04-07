cbdb1da08e0c06acb5e91e32e30908cde46c5461
samirdp04 <samirdelportillo01@gmail.com>
2026-04-07 18:27:15 -0500

    FIX - se eliminó la URL por defecto en API_BASE_URL para evitar conflictos en entornos de desarrollo

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8005/api';
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 // Crear instancia de axios
 const api = axios.create({
